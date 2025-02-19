41cbf3c4802dab39fc18c014c69d3fd38b6a4256
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-02-20 01:51:56 +0530

    Todo reducer export

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -22,6 +22,7 @@ export const todoSlice = createSlice({
     }
 })
 
+export const todoReducer = todoSlice.reducer
+
 export const {addTodo,removeTodo} = todoSlice.actions;
 
-export default todoSlice.reducer
